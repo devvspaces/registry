0084eb22b5a2d3dfbc157d520420b45d93d9d46c
devvspaces <netrobepy@gmail.com>
2021-11-18 00:58:55 +0100

    update

--- registry/registry/settings.py
+++ registry/registry/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-dx7-#u^_s7c$xvx-b7jukr&lrpsz+@u5b#-069v-d1$iap%7cp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['147.182.172.79']
 
 
 # Application definition
